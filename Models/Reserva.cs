501dbc184d5d1fefa45424b1d1786d0c642956fc
Gabriel <lopesg768@gmail.com>
2026-01-06 20:24:09 -0300

    Squashed 'trilha-net-explorando-desafio/' content from commit c9de4ce
    git-subtree-dir: trilha-net-explorando-desafio
    git-subtree-split: c9de4ce836b0f9daa1a39950c8829336772da70f

---
+++ Models/Reserva.cs
@@ -0,0 +1,44 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+                Hospedes = hospedes;
+            else
+                throw new Exception("Número de hóspedes é maior que a capacidade da suíte.");
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+                valor = valor - valor / 100 * 10;
+
+            return valor;
+        }
+    }
+}
